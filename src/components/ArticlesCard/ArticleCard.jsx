14c9572bb1d9ac0581bb2121662d48245ccf85d8
Beatriz <beatriz.pardington@gmail.com>
2023-12-12 13:08:49 +0000

    amend articlecard, click to view individual articl

--- src/components/ArticlesCard/ArticleCard.jsx
+++ src/components/ArticlesCard/ArticleCard.jsx
@@ -1,17 +1,45 @@
-function ArticleCard({article}){
-    return (
-       <>
-        <article >
-            <li><img 
+import { useNavigate } from "react-router-dom";
+
+function ArticleCard({ article }) {
+  const navigate = useNavigate();
+  return (
+    <>
+      <article className="article" key={article.article_id}>
+        <header className="article-header">
+          <li>
+            <h3 className="hover-title">
+              <a
+                onClick={() => {
+                  navigate(`/articles/${article.article_id}`);
+                }}
+                role="link"
+              >
+                {article.title}
+              </a>
+            </h3>
+          </li>
+
+          <li>
+            <p>{article.author}</p>
+          </li>
+        </header>
+
+        <li>
+          <img
+            onClick={() => {
+              navigate(`/articles/${article.article_id}`);
+            }}
             src={article.article_img_url}
             alt={`Image from ${article.title}`}
-            /></li>
-            <li><h3>{article.title}</h3></li>
-            <li><p>{article.author}</p></li>
-            <li><p>{article.topic}</p></li>
-        </article>
-        </>
-        )
+            role="link"
+          />
+        </li>
+        <li>
+          <p>{article.topic}</p>
+        </li>
+      </article>
+    </>
+  );
 }
 
-export default ArticleCard
+export default ArticleCard;
